e5658afcc27fe0019010dc02ad068a28fac09015
aryan9803 <aryanmirajkar7@gmail.com>
2025-12-01 15:25:47 +0530

    Update add_numbers.py

--- src/add_numbers.py
+++ src/add_numbers.py
@@ -4,7 +4,7 @@
 # Prompt the user to enter the first number
 num1 = float(input("Enter the first number: "))
 
-# Prompt the user to enter the second number
+# Prompt the user to enter the second numbers
 num2 = float(input("Enter the second number: "))
 
 # Add the two numbers
