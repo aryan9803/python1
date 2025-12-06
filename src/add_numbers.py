dd3fc64189da1fae32e36cb7b38d8b6e1609a140
aryan9803 <aryanmirajkar7@gmail.com>
2025-12-06 15:42:23 +0530

    Update add_numbers.py

--- src/add_numbers.py
+++ src/add_numbers.py
@@ -3,10 +3,10 @@ import os
 
 # Get numbers from environment variables
 num1 = float(os.getenv("NUM1", 0))
-num2 = float(os.getenv("NUM2", 0))
+num3 = float(os.getenv("NUM2", 0))
 
 # Add the numbers
-sum_result = num1 + num2
+sum_result = num1 + num3
 
 # Print the result
-print(f"The sum of {num1} and {num2} is {sum_result}")
+print(f"The sum of {num1} and {num3} is {sum_result}")
